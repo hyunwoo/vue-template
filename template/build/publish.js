5ec44a7964df1e6043d157ab85ece55680978e51
h.hyunwoo@gmail.com <Angles12>
2018-05-08 22:00:17 +0900

    v0.0.6

---
+++ template/build/publish.js
@@ -0,0 +1,16 @@
+const compression = require('compression');
+const express = require('express');
+const config = require('../config');
+const port = config.publish.port;
+const app = express();
+const cors = require('cors');
+
+app.use(require('connect-history-api-fallback')());
+app.use(compression());
+app.use(cors(config.publish.cors));
+
+const cacheDuration = 1000 * 60 * 60;
+app.use(express.static('./dist' , {maxAge: cacheDuration} ));
+app.use(express.static('./dataset' , {maxAge: cacheDuration} ));
+console.log('server start , port : ' + port);
+app.listen(port);
